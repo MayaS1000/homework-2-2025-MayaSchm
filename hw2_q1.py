108fcf36520292c6b3e78cc306f4caea912fa28a
MayaS1000 <schmeidler1@mail.tau.ac.il>
2025-04-21 13:20:39 +0300

    hw2_q1.py

--- hw2_q1.py
+++ hw2_q1.py
@@ -37,3 +37,15 @@ def english_to_morse(
         Name of output file containing the translated Morse code. Please don't change
         it since it's also hard-coded in the tests file.
     """
+    with open(input_file, 'r') as file:
+        text = file.read().upper()
+
+    # Replace each character by its Morse code representation
+    for char, morse_char in MORSE_CODE.items():
+        text = text.replace(char, morse_char + ' ')
+
+    # Split by spaces and join each word with a newline
+    morse_words = [' '.join(word.split()) for word in text.strip().split('   ')]
+
+    with open(output_file, 'w') as out_file:
+        out_file.write('\n'.join(morse_words))
